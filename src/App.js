9f1f3312156fd2bb51dce874547f69f3f736c030
Sergey Sherstobitov <in43sh@gmail.com>
2019-01-22 00:25:35 -0800

    working on front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/items').then(response => {
+      this.setState({items: response.data.items})
+      console.log(this.state.items);
+    })
+    .catch(error => console.log(error));
+  }
+
   render() {
+    const listOfItems = this.state.items.map(item => (
+      <div key={ item.id }>
+        Item name: <span>{ item.name }</span>
+      </div>
+    ))
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Basic React app</h1>
+        <div className="container">
+          <div className="inner-container">
+            { listOfItems }
+          </div>
+        </div>
       </div>
     );
   }
